cd995a9db698b136b7c7506f094ef61b84b0ef33
Santi Patiño <santiph2000@gmail.com>
2026-04-17 12:16:27 -0500

    Updates

---
+++ parcel.6e26ed7e.js
@@ -0,0 +1,8 @@
+const $app=document.getElementById("app"),API="https://api.escuelajs.co/api/v1/products?offset=5&limit=10",main=async()=>{let t=await fetch("https://api.escuelajs.co/api/v1/products?offset=5&limit=10"),a=await t.json(),e=a?.map(t=>`
+            <article class="Card">
+                <img src="${t.images[0]}" alt="${t.title}" />
+                <h2>${t.title}</h2>
+                <p>${t.price}</p>
+            </article>
+        `).join(""),i=document.createElement("section");i.classList.add("Items"),i.innerHTML=e,$app.appendChild(i)};main();
+//# sourceMappingURL=parcel.6e26ed7e.js.map
